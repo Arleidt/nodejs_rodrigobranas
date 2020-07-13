dba7601dad6a27d265a3f2412411473a7897e38b
arlei duarte <arleidt@yahoo.com.br>
2020-07-13 18:43:07 -0300

    aula 2

---
+++ nodebranas/module.js
@@ -0,0 +1,6 @@
+console.log(module.exports===exports);
+console.log(module.exports===this);
+console.log(exports===this);
+console.log(arguments);
+
+//apenas module.exports é retornado da função require
---
+++ nodebranas/serialGenerator.js
@@ -0,0 +1,29 @@
+var max = 10000
+
+//console.log(generate());
+//exports.generate = function (){ ou this.generate = function (){ também vão funcionar
+  module.exports.generate = function (){
+  return Math.floor(Math.random() * max);
+};
+
+//apenas module exports é retornado da função require, se alterar os outros ficarão desvinculados
+//var SerialGenerators = function(){
+  // varSerialGeneratos = function() com uma função construtora
+  //   this.generate=function()
+  // module.exports = new SerialGenerator()
+
+   /* this.generates = function (){
+      return Math.floor(Math.random() * max);
+    };
+    return{
+      generates : generates
+    };
+}
+module.exports = SerialGenerators;**/
+    //exportando por meio de objetos
+    /*module.exports = {
+      generates: generates
+    };
+    desse jeito perdi a referencia para exports e tudo que estiver acima nao preciso colocar dentro
+    e this ou só exports n funciona pq o que é retornado é module exports
+    **/
