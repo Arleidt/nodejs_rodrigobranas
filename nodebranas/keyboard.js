dced50e3bbd0c32be0a3cd111f08dcfac386db7e
arlei duarte <arleidt@yahoo.com.br>
2020-07-15 15:56:11 -0300

    aula 5 continuação

--- nodebranas/keyboard.js
+++ nodebranas/keyboard.js
@@ -1,12 +1,11 @@
 //stdin para ler teclado
 exports.onReadable = function(callback){
-  
-//detecta mudança alguma coisa pra ler vai ler se tiver algo la diferente de null vai invocar meu callback convertendo chunk para string
-        process.stdin.on('readable', function(){
-            var chunk = process.stdin.read();
-            if(chunk) callback(chunk.toString());
-            //if (chunk)  console.log(chunk.toString());
-            //if (chunk != null)  console.log(chunk.toString());
-            //buffer é muito cru vantagem que posso converter com base 64 com sistema de codificação diferente
+  //detecta mudança alguma coisa pra ler vai ler se tiver algo la diferente de null vai invocar meu callback convertendo chunk para string
+   process.stdin.on('readable', function(){
+      var chunk = process.stdin.read();
+      if(chunk) callback(chunk.toString().replace(/\n/, ' '));
+      //if (chunk)  console.log(chunk.toString());
+      //if (chunk != null)  console.log(chunk.toString());
+      //buffer é muito cru vantagem que posso converter com base 64 com sistema de codificação diferente
     });
 }
