e3049b2158d6854785eecae77abacd9bdaac5bd8
arlei duarte <arleidt@yahoo.com.br>
2020-07-16 16:34:15 -0300

    aula 6 branas

---
+++ nodebranas/client.js
@@ -0,0 +1,10 @@
+var net = require('net');
+
+var client = net.connect(3000);
+client.on('data', function(message){
+    console.log(message.toString());
+});
+/**net.createConnection({host: '127.0.0.1', port: 3000});
+Eventos: um dos + importantes é o evento data, responsável por detectar o recebimento de novas msgs
+da disponibilidade de dados nessa conexão então a gnt vai receber
+nesse caso só exibindo no console*/
