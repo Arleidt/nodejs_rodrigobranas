caf3269773590d1410c1e4706f1ff942ccbd4ef6
arlei duarte <arleidt@yahoo.com.br>
2020-07-16 23:12:45 -0300

    aula 6 branas client server bi direcional

--- nodebranas/client.js
+++ nodebranas/client.js
@@ -1,9 +1,21 @@
 var net = require('net');
 
 var client = net.connect(3000);
+client.on('connect', function(){
+    client.write('Hello, I am the client!'); //servidor bi direcional
+});
 client.on('data', function(message){
     console.log(message.toString());
 });
+
+
+/**var net = require('net');
+
+var client = net.connect(3000);
+client.on('data', function(message){
+    console.log(message.toString());
+});*/
+
 /**net.createConnection({host: '127.0.0.1', port: 3000});
 Eventos: um dos + importantes é o evento data, responsável por detectar o recebimento de novas msgs
 da disponibilidade de dados nessa conexão então a gnt vai receber
--- nodebranas/server.js
+++ nodebranas/server.js
@@ -1,5 +1,8 @@
 var net = require('net');
 
 net.createServer(function (connection){
-    connection.write('Hello, I am the server!')
+    connection.write('Hello, I am the server!');
+    connection.on('data', function(message){
+        console.log(message.toString());//servidor bi direcional
+    });
 }).listen(3000);
